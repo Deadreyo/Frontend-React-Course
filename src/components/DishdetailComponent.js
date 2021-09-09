69474f5390947d45173d16ce8dc098401a971374
NightKnight <Ahmedatwa866@yahoo.com>
2021-09-09 14:42:44 +0200

    functional comp

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,53 +2,55 @@ import { Component } from "react";
 import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
 
 
-export class DishDetail extends Component {
 
 
-    render() {
-        if(!this.props.dish) return <div></div>
-        
-        else return(
+const DishDetail = (props) => {
+    if(!props.dish) return <div></div>
+    
+    else return(
+        <div className="container">
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.dish)}
+                    {renderDish(props.dish)}
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    {this.renderComments(this.props.dish.comments)}
+                    {renderComments(props.dish.comments)}
                 </div>
             </div>
-        )
-    }
-
-    renderDish(dish) {
-        return (
-            <Card>
-                <CardImg top src={dish.image} alt={this.props.dish.name} width="100%"/>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        )
-    }
-
-    renderComments(comments) {
-        if(!comments) return <div></div>
-
-        const commentData = comments.map( (com) => {
-            return(
-                <ul className="list-unstyled">
-                    <li>{com.comment}</li>
-                    <li>-- {com.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(com.date)))}</li>   
-                </ul>
-            )
-        })
-
-        return(
-            <div>
-                <h4>Comments</h4>
-                {commentData}
-            </div>
-        )
-    }
+        </div>
+    )
 }
+
+function renderDish(dish) {
+    return (
+        <Card>
+            <CardImg top src={dish.image} alt={dish.name} width="100%"/>
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
+
+function renderComments(comments) {
+    if(!comments) return <div></div>
+
+    const commentData = comments.map( (com) => {
+        return(
+            <ul className="list-unstyled">
+                <li>{com.comment}</li>
+                <li>-- {com.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(com.date)))}</li>   
+            </ul>
+        )
+    })
+
+    return(
+        <div>
+            <h4>Comments</h4>
+            {commentData}
+        </div>
+    )
+}
+
+export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import { DISHES } from "../shared/dishes";
-import { DishDetail } from "./DishdetailComponent";
+import DishDetail from "./DishdetailComponent";
 import Menu from "./MenuComponent";
 
 class Main extends Component {
